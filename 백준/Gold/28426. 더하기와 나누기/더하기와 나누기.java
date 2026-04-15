9ab39592af8b4235355255eaea5aa2632e66f3a7
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 19:31:07 +0900

    [Gold V] Title: 더하기와 나누기, Time: 1236 ms, Memory: 33184 KB -BaekjoonHub

---
+++ 백준/Gold/28426. 더하기와 나누기/더하기와 나누기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer a = sc.nextInt();
+        System.out.print(2);
+
+          for (int t = 1; t < a; t++){
+             System.out.print(" " + (t * 4));
+            }
+      }
+}
