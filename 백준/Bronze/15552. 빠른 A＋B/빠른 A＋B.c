940a1c850d952dd31b052a96f8c83e05a11c93f5
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 19:26:48 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 356 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a = 0, b = 0, c = 0;
+	scanf("%d", &a);
+	for (int i = 0; i < a; i++) {
+		scanf("%d %d", &b, &c);
+		printf("%d\n", b + c);
+	}
+
+	return 0;
+}
