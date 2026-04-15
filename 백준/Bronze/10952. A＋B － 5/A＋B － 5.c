d47911346cc12d130498ac41b230d41ca4d4d045
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 17:46:54 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -3,7 +3,8 @@ int main() {
 	int a, b;	
 	for (;;) {
         scanf("%d %d", &a, &b);
-        if(a == 0 && b == 0) return 0;
+        if(a == 0 && b == 0) break;
         printf("%d\n", a + b);
     }
+	return 0;
 }
