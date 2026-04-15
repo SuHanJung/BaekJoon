7081e4a601ae2fb6ecb06a10db95edb8e908ba88
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 19:14:54 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -1,7 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
 int main() {
-	char n;
-	int s[26], t;
-	for (t = 0; t < 26; t++)s[t] = -1;
-	for (t = 0, n = getchar(); n >= 'a'; n = getchar(), t++)if (s[n - 'a'] == -1) s[n - 'a'] = t;
-	for (t = 0; t < 26; t++) printf("%d ", s[t]);
+    int a = 0, b[26] = { 0 };
+    char p[101] = { 0 }, q = 'a';
+    scanf("%s", &p);
+    for (int i = 0, k = 0; p[i] != 0; i++) {
+        k = p[i] - q;
+        if (b[k] == 0) {
+            b[k] = i + 1;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        printf("%d ", b[i] - 1);
+    }
+    return 0;
 }
