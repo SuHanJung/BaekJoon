0f2956bc18d7db6c2adb3fa57f11d189a4baea14
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 17:36:52 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-int main() {
-		int a, b, c = 0;
-		scanf("%d", &a);
-		b = a--;
-		while (1) {
-			while(0 < b - a - ++c) printf(" ");
-			while(0 <= b - c++) printf("*");
-			if(--a < 0) return 0;
-			printf("\n");
-			c = 0;
-		}
+int main(){
+    int a;
+    scanf("%d", &a);
+    for(int b = a;a > 0;a--){
+        for(int t = 0;t< b - a;t++) printf(" ");
+        for(int t = 0;t<a;t++) printf("*");
+        printf("\n");
+    }
+    return 0;
 }
