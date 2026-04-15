3516cb2c04944ca6bcd1d0bd97de5898d6390836
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 18:33:02 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int factorial(int n) {
+	int ans = 1;
+	for (int time = 1; time <= n; time++) ans *= time;
+	return ans;
+}
+
+int main() {
+	int n, r;
+	scanf("%d %d", &n, &r);
+	printf("%d", (factorial(n) / factorial(r)) / factorial(n - r));
+	return 0;
+}
