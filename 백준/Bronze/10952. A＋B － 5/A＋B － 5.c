e9b2a4dfb662a2233a76fc795e03858664502afb
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 19:21:39 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -1,10 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 int main() {
-	int a, b;	
-	for (;;) {
-        scanf("%d %d", &a, &b);
-        if(a == 0 && b == 0) break;
-        printf("%d\n", a + b);
-    }
+	int a = 1, b = 1;
+	
+	for (int i = 1; i = 55;) {
+		scanf("%d %d", &a, &b);
+		if (a != 0) {
+			printf("%d\n", a + b);
+		}
+		else
+			if (b != 0) {
+				printf("%d\n", a + b);
+			}
+			else break;
+	}
+
 	return 0;
 }
