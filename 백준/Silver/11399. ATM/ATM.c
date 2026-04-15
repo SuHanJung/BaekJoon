29e4ad89418a4a4cd43b824c381516c12fdd395f
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 18:08:12 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int compare(int a, int b) {
+	if (a > b) return b;
+	else return a;
+}
+int sorting(int input[], int n, int size) {
+	int l = size / 2, * instarr;
+	instarr = malloc(sizeof(int) * n);
+	for (int time = 0; time + 1 <= n / size; time++) {
+		for (int sa = time * size, sb = time * size + l, move = 0; move < size;) {
+			if (sa == time * size + l) {
+				instarr[time * size + move] = input[sb];
+				sb++;
+			}
+			else {
+				if (sb == (time + 1) * size) {
+					instarr[time * size + move] = input[sa];
+					sa++;
+				}
+				else {
+					if (compare(input[sa], input[sb]) == input[sa]) {
+						instarr[time * size + move] = input[sa];
+						sa++;
+					}
+					else {
+						instarr[time * size + move] = input[sb];
+						sb++;
+					}
+				}
+			}
+			move++;
+		}
+	}
+	if (n % size > l) {
+		for (int sa = (n / size) * size, sb = (n / size) * size + l, move = 0; move < n % size;) {
+			if (sa == (n / size) * size + l) {
+				instarr[(n / size) * size + move] = input[sb];
+				sb++;
+			}
+			else {
+				if (sb == n) {
+					instarr[(n / size) * size + move] = input[sa];
+					sa++;
+				}
+				else {
+					if (compare(input[sa], input[sb]) == input[sa]) {
+						instarr[(n / size) * size + move] = input[sa];
+						sa++;
+					}
+					else {
+						instarr[(n / size) * size + move] = input[sb];
+						sb++;
+					}
+				}
+			}
+			move++;
+		}
+	}
+	else {
+		for (int i = (n / size) * size; i < n; i++) instarr[i] = input[i];
+	}
+	for (int i = 0; i < n; i++) input[i] = instarr[i];
+	free(instarr);
+	return 0;
+}
+
+int Jsort(int input[], int n) {
+	for (int i = 1; i < n; i *= 2) {
+		sorting(input, n, 2 * i);
+	}
+}
+
+int main() {
+	int n = 0,*arr, ans = 0;
+	scanf("%d", &n);
+	arr = (int*)malloc(sizeof(int) * n);
+	for (int t = 0; t < n; t++) scanf("%d", &arr[t]);
+	Jsort(arr, n);
+	for (int t = n; t > 0; t--) ans += t * arr[n - t];
+	printf("%d", ans);
+	return 0;
+}
