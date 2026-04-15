bdcd85a448dded374666da2e46222f0a8aa0696a
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 17:12:06 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	cout << (1 << N);
+	return 0;
+}
