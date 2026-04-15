8b8f14e5c25e2adaa6a480260ce375d7e8ea8419
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 19:20:18 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.c
+++ 백준/Bronze/2562. 최댓값/최댓값.c
@@ -1,9 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
 int main() {
-	int a[10], *m = &a[1];
-	for (int t = 1, n; t < 10; t++) {
-		scanf("%d", &a[t]);
-		m = (a[t] > *m) ? &a[t]:m;
-}
-	printf("%d\n%d", *m, m - a);
+	int a = 0, b = 0;
+	for (int i = 1, p = 0; i < 10; i++) {
+		scanf("%d", &p);
+		if (p > b) {
+			b = p;
+			a = i;
+		}
+	}
+	printf("%d\n%d", b, a);
 	return 0;
 }
