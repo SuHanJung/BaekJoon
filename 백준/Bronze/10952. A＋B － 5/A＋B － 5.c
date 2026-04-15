d2b5ed6fb01387a915d2c821dbe35469fec232d6
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 17:44:13 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -1,10 +1,9 @@
-#include<stdio.h>
-int main(){
-    int a, b;
-    scanf("%d %d", &a, &b);
-    while(a != 0 && b != 0){
-        printf("%d\n", a + b);
+#include <stdio.h>
+int main() {
+	int a, b;	
+	for (;;) {
         scanf("%d %d", &a, &b);
+        if(a == 0 && b == 0) return 0;
+        printf("%d\n", a + b);
     }
-    return 0;
 }
