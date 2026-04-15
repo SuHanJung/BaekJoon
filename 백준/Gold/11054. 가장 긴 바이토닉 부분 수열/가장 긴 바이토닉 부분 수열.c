9b1eac3ecb6eabc37d27a999a2dbd5921127669d
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 17:31:05 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.c
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.c
@@ -1,10 +1,12 @@
-int main() {
-	int N, max = 0, arr[1000][3] = { 0 };
-	scanf("%d", &N);
-	for (int t = 0; t < N; t++) scanf("%d", &arr[t][0]);
-	for (int t = 0; t < N; t++) for (int tt = 0; tt < t; tt++) if (arr[tt][0] < arr[t][0] && arr[tt][1] + 1 > arr[t][1]) arr[t][1] = arr[tt][1] + 1;
-	for (int t = N - 1; t >= 0; t--) for (int tt = N - 1; tt > t; tt--) if (arr[tt][0] < arr[t][0] && arr[tt][2] + 1 > arr[t][2]) arr[t][2] = arr[tt][2] + 1;
-	for (int t = 0; t < N; t++) if (max < arr[t][1] + arr[t][2]) max = arr[t][1] + arr[t][2];
-	printf("%d", max + 1);
-	return 0;
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
