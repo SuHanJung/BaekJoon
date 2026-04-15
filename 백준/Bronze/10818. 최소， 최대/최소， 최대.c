020eae8ca70f04cac63ada5b47ac488d7fd94769
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 17:35:26 +0900

    [Bronze III] Title: 최소, 최대, Time: 156 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.c
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -1,6 +1,8 @@
+#include<stdio.h>
 int main(){
-    int N, Max = -1000000, Min = 1000000, input;
-    for(scanf("%d",&N);N > 0;N--,scanf("%d",&input),Max=(input>Max)?input:Max,Min=(input<Min)?input:Min);
-    printf("%d %d",Min,Max);
+    int N, Max = -1000000, Min = 1000000;
+    scanf("%d", &N);
+    for(int input;N > 0;N--, scanf("%d", &input), Max = (input > Max) ? input:Max, Min = (input < Min) ? input:Min);
+    printf("%d %d", Min, Max);
     return 0;
 }
