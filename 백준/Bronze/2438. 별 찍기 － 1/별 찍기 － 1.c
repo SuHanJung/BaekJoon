e7ec00ca606cde8bdad65dbf9dcdfe13056e90ab
JSH <145888390+SuHanJung@users.noreply.github.com>
2026-04-15 19:22:19 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -1,14 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main(){
-    int N, T = 1;
-    scanf("%d", &N);
-    for(int t = 1;T <= N;t++) {
-        printf("*");
-        if(t == T){
-            T++;
-            t = 0;
-            printf("\n");
-        }
-    }
-    return 0;
+
+int main() {
+	int a = 0;
+	scanf("%d", &a);
+	
+	for (int i = 1; i <= a; i++) {
+		for (int p = 0; p < i; p++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
